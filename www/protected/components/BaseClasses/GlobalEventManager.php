2383b87fa284412a2cdc0e804d5b50becf890e71
nizsheanez <www.pismeco@gmail.com>
2011-08-13 15:48:53 +0600

    add GlobalEventManager using examples

--- www/protected/components/BaseClasses/GlobalEventManager.php
+++ www/protected/components/BaseClasses/GlobalEventManager.php
@@ -2,6 +2,9 @@
 /**
  * registration events
  * all events in CMS must be incapsulated there
+ * Use:
+ * 1. setUp - Y::events()->event = function($event) {};
+ * 2. call - Y::events()->event($params);
  */
 class GlobalEventManager extends CComponent
 {

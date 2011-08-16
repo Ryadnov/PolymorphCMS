1b4a72c112371fde8b9af0bd1e7e4af73258538d
nizsheanez <www.pismeco@gmail.com>
2011-08-16 12:17:00 +0600

    widgets not registering yet

--- www/protected/components/BaseClasses/ResourceManager.php
+++ www/protected/components/BaseClasses/ResourceManager.php
@@ -4,25 +4,7 @@
  */
 class ResourceManager extends CApplicationComponent
 {
-    private $_w;
     private $_p;
-
-    /**
-     * register all widgets belonging to blocks of this and parrents categories
-     * @param Category $category
-     * @return void
-     */
-    public function registerWidgets($category)
-    {
-        foreach ($category->allBlocks as $block) {
-            foreach ($block['block']->widgets as $widget) {
-                Yii::import('widgets.'.$widget->class.'.*');
-                $w = Yii::app()->getWidgetFactory()->createWidget(Y::controller(),$widget->class, $widget->settings);
-                $w->register($category);
-                $this->_w[] = $widget->pk;
-            }
-        }
-    }
 
     public function registerPlugins()
     {

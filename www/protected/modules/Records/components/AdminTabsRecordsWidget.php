f76475bd145d64f646a84a4e832a437fa91c5c00
unknown <Alex@.(none)>
2011-08-18 16:40:56 +0600

    unfail commit

---
+++ www/protected/modules/Records/components/AdminTabsRecordsWidget.php
@@ -0,0 +1,17 @@
+<?php
+class AdminTabsRecordsWidget extends Widget
+{
+    public $model;
+    public $form;
+
+    public function update()
+    {
+           
+    }
+
+    public function renderContent()
+    {
+        $this->render('_adminTabs', array('model'=>$this->model, 'form'=>$this->form));
+    }
+    
+}
---
+++ www/protected/modules/Records/config.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'class'=>'modules.records.RecordsModule'
+);
